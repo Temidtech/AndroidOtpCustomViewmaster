70b807136e900addab719d6b6b4a443338d0d2da
Temidayo Adefioye <temidjoy@hotmail.com>
2018-05-11 16:25:57 +0100

    add_screenshot

---
+++ app/src/main/java/com/swiftsynq/android_otpcustomview_master/OtpScreen.kt
@@ -0,0 +1,15 @@
+package com.swiftsynq.android_otpcustomview_master
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by popoolaadebimpe on 11/05/2018.
+ */
+class OtpScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.otp_screen)
+    }
+}
